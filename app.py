67439486d08f39ac230ee10bef86c33200c693b9
Lekshmi <lekshmi2208@gmail.com>
2026-09-18 10:24:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to My Flask Application!"
+    return "Hello, GitHub Actions!"
 
 @app.route("/about")
 def about():
-    return "This is a Python Flask application using GitHub Actions."
-
-@app.route("/contact")
-def contact():
-    return "Contact page of my Flask application."
+    return "This is my Flask application."
 
 if __name__ == "__main__":
     app.run(debug=True)
